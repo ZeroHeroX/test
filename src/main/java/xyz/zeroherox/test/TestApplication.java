9f70ba706dcbad1c16cb30859a0ce4dc7aff972f
ZeroHeroX <redheartk@foxmail.com>
2025-05-08 20:45:30 +0800

    Initial commit

---
+++ src/main/java/xyz/zeroherox/test/TestApplication.java
@@ -0,0 +1,27 @@
+package xyz.zeroherox.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+
+    }
+
+
+    @RestController
+    public static class TestController {
+
+        @RequestMapping("/")
+        public String hello() {
+            return "Hello World!";
+        }
+
+    }
+
+}
